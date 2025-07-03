3ab78c57bf19d8407f8258d27393fc7866766dea
Deep <droy00524@gmail.com>
2025-07-03 09:27:03 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -8,6 +8,33 @@ dotenv.config({
 })
 
 connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at: ${process.env.PORT}`);
+            
+        })
+    })
+    .catch((error) => {
+    console.log("MongoDB connection failed !!!", error);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import express from 'express'
 // const app = express()
 // (async () => {
